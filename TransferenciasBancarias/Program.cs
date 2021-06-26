b0a9916315b3daeb4c4522c43a263d7a01d5f2b1
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-26 20:54:47 -0300

    Inicia Projeto de Transferências Bancárias .NET C#

---
+++ TransferenciasBancarias/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransferenciasBancarias
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
