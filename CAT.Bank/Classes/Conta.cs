fb862d0c1d5d4d38332895e014d42536219feaa8
Jeferson Pontes <jeffersonpontes1313@gmail.com>
2021-06-28 10:34:50 -0300

    Cria Enum TipoConta e Construtor de Conta

---
+++ CAT.Bank/Classes/Conta.cs
@@ -0,0 +1,21 @@
+﻿using CAT.Bank.Enums;
+
+namespace CAT.Bank.Classes
+{
+    public class Conta
+    {
+        private TipoConta TipoConta {get; set; }
+        private string Nome { get; set; }
+        private double Saldo {get; set; }
+        private double Credito {get; set; }
+
+
+        public Conta(TipoConta tipoConta, string nome, double saldo, double credito)
+        {
+            this.TipoConta = tipoConta;
+            this.Nome = nome;
+            this.Saldo = saldo;
+            this.Credito = credito;
+        }
+    }
+}
---
+++ CAT.Bank/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAT.Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
